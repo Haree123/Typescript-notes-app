05fe917ae5ed70f69ff98a70a3a6cf948ea9d710
Haree <haree@addup.network>
2023-12-07 20:53:55 +0530

    updated yml file and removed homepage

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Typescript-notes-app/",
   plugins: [react()],
-  base: "/Typescript-notes-app/"
-})
+});
